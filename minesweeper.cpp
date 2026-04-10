c6112a11ff8fd3bd533dc25aaa9589d4288151a0
NaN <NanJiang07@gmail.com>
2026-04-10 23:22:12 +0800

    Add files via upload
    Some initial functions and variables defined for minesweeper game

---
+++ minesweeper.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <cctype>
+using namespace std;
+
+const int easygrid = 5;
+const int mediumgrid = 10;
+const int hardgrid = 15;
+const int easymine = 5;
+const int mediummine = 10;
+const int hardmine = 20;
+
+class Minesweeper {
+public:
+    Minesweeper(int size, int mines);
+    void displayBoard();
+    void clearScreen();
+
+private:
+    void initializeGrids(int size);
+    int size;
+    int mines;
+    vector<vector<bool>> mineGrid;
+    vector<vector<int>> solutionGrid;
+    vector<vector<char>> revealedGrid;
+    bool gameOver;
+    bool win;
+};
+
+void clearScreen(){
+    for(int i=0; i<100; i++){
+        cout << endl;
+    }
+}
+
+char toUpper(char c){
+    return toupper(c);
+}
+
+bool isValidMove(int x, int y, int gridSize) {
+    if (x < 0 || x >= gridSize || y < 0 || y >= gridSize) {
+        return false;
+    }
+    return true;
+}
+
+int countAdjacentMines(int x, int y, const vector<vector<int>>& board) {
+    int count = 0;
+    for (int i = -1; i <= 1; ++i) {
+        for (int j = -1; j <= 1; ++j) {
+            if (isValidMove(x + i, y + j, board.size()) && board[x + i][y + j] == -1) {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+void displayBoard(const vector<vector<int>>& grid, const vector<vector<bool>>& revealed) {
+    cout << "   ";
+    for (int i = 0; i < grid.size(); ++i) {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    for (int i = 0; i < grid.size(); ++i) {
+        cout << i << " ";
+        for (int j = 0; j < grid[i].size(); ++j) {
+            if (revealed[i][j]) {
+                if (grid[i][j] == -1) {
+                    cout << "* ";
+                } else {
+                    cout << countAdjacentMines(i, j, grid) << " ";
+                }
+            } else {
+                cout << ". ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+void initializeGrids(int size){
+    mineGrid.assign(size, vector<bool>(size, false));
+    solutionGrid.assign(size, vector<int>(size, 0));
+    revealedGrid.assign(size, vector<char>(size, '#'));
+}
+
+void placeMines(){
+    int placedMines = 0;
+    while (placedMines < mines) {
+        int x = rand() % size;
+        int y = rand() % size;
+        if (!mineGrid[x][y]) {
+            mineGrid[x][y] = true;
+            placedMines++;
+        }
+    }
+}
