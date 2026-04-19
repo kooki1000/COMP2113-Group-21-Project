4931ff8203b5e52195c6ffee12e4391c2a08f3c1
NaN <NanJiang07@gmail.com>
2026-04-19 20:59:41 +0800

    Added minesweeper header file

---
+++ minesweeper.h
@@ -0,0 +1,43 @@
+#ifndef MINESWEEPER_H
+#define MINESWEEPER_H
+
+#include <vector>
+#include <chrono>
+
+class Minesweeper {
+public:
+    Minesweeper(int size, int mines);
+    void playGame();
+
+private:
+    int size;
+    int mines;
+    std::vector<std::vector<bool>> mineGrid;
+    std::vector<std::vector<int>> solutionGrid;
+    std::vector<std::vector<char>> revealedGrid;
+    bool gameOver;
+    bool win;
+    std::chrono::steady_clock::time_point startTime;
+    void initializeGrids(int size);
+    void placeMines();
+    void fillSolutionGrid();  
+    bool isValidMove(int x, int y);
+    int countAdjacentMines(int x, int y);
+    void revealSingleCell(int x, int y);
+    void floodReveal(int x, int y);
+    void flagCell(int x, int y);
+    bool checkWin();
+    void revealMines();
+    void displayBoard();
+    void clearScreen();
+    void getPlayerInput();
+    void makeMove(char action, int x, int y);
+    char toUpper(char c);
+    double getElapsedTime();
+    void showHighScore();
+    void saveHighScore(double time);
+    void gameOverMessage();
+
+};
+
+#endif
