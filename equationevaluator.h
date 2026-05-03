67ca4c89a65ad8b10e2d775058b94ca3ad248739
NaN <NanJiang07@gmail.com>
2026-05-03 22:24:28 +0800

    Refactor evaluator class definition and formatting

--- equationevaluator.h
+++ equationevaluator.h
@@ -27,19 +27,24 @@
 
 #ifndef EQUATIONEVALUATOR_H
 #define EQUATIONEVALUATOR_H
+
 #include <map>
 #include <string>
 #include <vector>
-class evaluator{
-    public:
+
+class evaluator {
+public:
     double evaluate(const std::string& expression);
     bool checkNumbersUsed(const std::string& expression, std::vector<int> numbers);
-    private:
+
+private:
     std::map<char, int> precedence = {
-            {'+', 1}, {'-', 1}, {'*', 2}, {'/', 2}
+        {'+', 1}, {'-', 1}, {'*', 2}, {'/', 2}
     };
+
     bool isOperator(char c);
     std::vector<std::string> shunting_yard(const std::string& expression);
     double evaluate_rpn(const std::vector<std::string>& rpn);
 };
+
 #endif
